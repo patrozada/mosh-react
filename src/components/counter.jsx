2bad8529e8c26816a741e2ce0197e5b8e76e6c49
patrozada <patriciarozada@gmail.com>
2020-09-17 19:03:21 +0200

    Render all elements in a list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 56,
+        tags: ['tag1', 'tag2', 'tag3'],
     }
     render() { 
         return (
         <>
             <span className={ this.getBadgeChanges() }>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {this.state.tags.map(tag=> <li key={tag}> {tag} </li>)}
+            </ul>
         </>)
     }
     formatCount () {

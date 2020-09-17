0a7fc3ca397d5c0ec250dbcec7a3829ec4f3abaf
patrozada <patriciarozada@gmail.com>
2020-09-17 18:52:42 +0200

    Add method to assign classes dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 56,
+    }
     render() { 
-        return <h1>Hello world!</h1>
+        return (
+        <>
+            <span className={ this.getBadgeChanges() }>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </>)
+    }
+    formatCount () {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+    getBadgeChanges(){
+        let classes = "badge m-3 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 }
  
